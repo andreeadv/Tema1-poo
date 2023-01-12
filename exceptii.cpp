b92c6b183eeaf62eb9e666d55b31bfcc83d18671
andreeadv <andreeavioletadinu@gmail.com>
2023-01-12 22:27:46 +0200

    Tema2_final

--- exceptii.cpp
+++ exceptii.cpp
@@ -1,8 +1,8 @@
 #include "exceptii.h"
 
 
-eroare_angajat::eroare_angajat(const std::string &mesaj): eroare_aplicatie("Eroare Angajat: " + mesaj){}
+eroare_angajat::eroare_angajat(const std::string &mesaj): eroare_aplicatie(mesaj){}
 
-eroare_comanda::eroare_comanda(const std::string &mesaj):eroare_aplicatie("Eroare Comanda: " + mesaj){}
+eroare_comanda::eroare_comanda(const std::string &mesaj):eroare_aplicatie(mesaj){}
 
-eroare_plata::eroare_plata(const std::string &mesaj):eroare_aplicatie("Eroare Plata: " + mesaj){}
+eroare_plata::eroare_plata(const std::string &mesaj):eroare_aplicatie(mesaj){}
